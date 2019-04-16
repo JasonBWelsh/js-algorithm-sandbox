64b1efd9cda09c6beb402362328d92f3c973a32b
Jason Welsh <jasonbwelsh@gmail.com>
2019-04-15 23:58:28 -0400

    First solution for max character problem

--- js1.js
+++ js1.js
@@ -1,12 +1,26 @@
 // Reverse integer passed
-function reverseInt(int) {
-  const revString = int
-    .toString()
-    .split("")
-    .reverse()
-    .join("");
+// function reverseInt(int) {
+//   const revString = int
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .join("");
 
-  return parseInt(revString) * Math.sign(int);
+//   return parseInt(revString) * Math.sign(int);
+// }
+
+// Capitalize letters
+function CapitalizeLetters(str) {
+  const x = str
+    .toLowerCase()
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.substring(1));
+  return x;
 }
 
-console.log("output\n", reverseInt(-125));
+// Return max character in string (most common)
+// function maxCharacter(str) {
+//   return str;
+// }
+
+console.log("output\n", CapitalizeLetters("I am a VERY long string to pass"));

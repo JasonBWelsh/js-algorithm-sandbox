3b9d0ba29ee5c0d50b478f28629c3f1ff28fe1ae
Jason Welsh <jasonbwelsh@gmail.com>
2019-04-16 19:49:51 -0400

    correction

--- js1.js
+++ js1.js
@@ -48,7 +48,7 @@
 function fizzBuzz() {
   let i = 1;
   while(i <= 100) {
-    if(i % 15 === 0) {
+    if(i % 3 === 0 && i % 5 === 0) {
       console.log('fizzBuzz');
     } else if(i % 3 === 0) {
       console.log('fizz');

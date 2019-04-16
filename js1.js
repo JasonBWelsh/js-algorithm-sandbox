df6f9bf6d44240e03a3242cbcc63ff6e59e22bd4
Jason Welsh <jasonbwelsh@gmail.com>
2019-04-15 23:38:03 -0400

    ReverseInt v1

--- js1.js
+++ js1.js
@@ -1,1 +1,12 @@
-const x;
+// Reverse integer passed
+function reverseInt(int) {
+  return parseInt(
+    int
+      .toString()
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
+
+console.log("output\n", reverseInt(125));

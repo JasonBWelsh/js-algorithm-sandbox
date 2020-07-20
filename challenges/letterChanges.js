23ecf33cdb3001617fcb13cc9679e6ad374d0279
Jason Welsh <jasonbwelsh@gmail.com>
2020-07-20 11:55:38 -0400

    added capitalize vowels to challenge

--- challenges/letterChanges.js
+++ challenges/letterChanges.js
@@ -3,10 +3,16 @@ Change outpu to be each letter passed in + 1
 ex: 'hello there' is 'ifmmp uifsf'
 */
 
+// capitalize vowels
+
 function letterChanges(str) {
-  const newString = str.toLowerCase().replace(/[a-z]/gi, function (char) {
+  let newString = str.toLowerCase().replace(/[a-z]/gi, function (char) {
     if (char === 'z') return 'a';
     return String.fromCharCode(char.charCodeAt() + 1);
+  });
+
+  newString = newString.replace(/a|e|i|o|u/gi, function (vowel) {
+    return vowel.toUpperCase();
   });
 
   return newString;

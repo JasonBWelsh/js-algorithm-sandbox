670d286f377e5d15bbdb3396668e0ea854d676a0
Jason Welsh <jasonbwelsh@gmail.com>
2020-07-21 19:17:00 -0400

    seekAndDestroy solution 2

--- challenges/seekAndDestroy.js
+++ challenges/seekAndDestroy.js
@@ -1,13 +1,19 @@
-function destroyer(arr, ...args) {
-  const survivors = [];
+// function destroyer(arr, ...args) {
+//   const survivors = [];
 
-  for (let num of arr) {
-    if (args.indexOf(num) === -1) {
-      survivors.push(num);
-    }
-  }
+//   for (let num of arr) {
+//     if (args.indexOf(num) === -1) {
+//       survivors.push(num);
+//     }
+//   }
 
-  return survivors;
+//   return survivors;
+// }
+
+// solution 2
+
+function destroyer(arr, ...targets) {
+  return arr.filter((num) => !targets.includes(num));
 }
 
 const targetArray = [1, 2, 1, 3, 1, 1, 3, 2];
